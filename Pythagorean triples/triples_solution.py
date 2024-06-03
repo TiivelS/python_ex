746c6ef2a0544c812ae742346f47e0d10d2733bf
TiivelS <148441701+TiivelS@users.noreply.github.com>
2024-06-03 15:02:39 +0300

    Update triples_solution.py
    Fixed typo

--- Pythagorean triples/triples_solution.py
+++ Pythagorean triples/triples_solution.py
@@ -7,7 +7,7 @@ Parameters:
 limit (int): The upper limit for the values of a, b, and c.
 
 Returns:
-list of tuple: A list of tuples, each containing a Pythagorean triple (a, b, c).
+list of tuple of int: A list of tuples, each containing a Pythagorean triple (a, b, c).
 
 Ensure that a < b < c.
 
@@ -15,7 +15,7 @@ Ensure that the tuples are sorted ascended based on the first element, then the 
 and then the third element if the first two elements are equal.
 
 Example:
-If limit == 10, the expected result is [(3, 4, 5), (6, 8, 10)], since all the members of the tuples are under the limit and a^2 + b^2 = c^2.
+If limit == 10, the expected result is [(3, 4, 5), (6, 8, 10)], since all the members of the tuples are under the limit or equal to the limit and a^2 + b^2 = c^2.
 The tuples are also sorted correctly and a < b < c.
 """
 def find_pythagorean_triples(limit):
